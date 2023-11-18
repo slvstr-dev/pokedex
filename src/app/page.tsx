3c61fd269495dca11011f9b6ae8d1565702c4fdb
Sylvester <hello@slvstr.dev>
2023-11-18 13:04:22 +0100

    Update page styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from 'next/image';
-
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-2xl font-semibold">Hello Kanto region!</h1>
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-semibold">Hello Kanto region!</h1>
     </main>
   );
 }
