5da20fa144c867216afdd599dfd692caebb86c06
Sylvester <hello@slvstr.dev>
2023-11-18 12:59:36 +0100

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -8,13 +8,18 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        body: ['var(--font-inter)'],
+      },
+      container: {
+        center: true,
+        padding: '1rem',
+      },
+      screens: {
+        xs: '480px',
       },
     },
   },
-  plugins: [],
-}
-export default config
+  tailwindFunctions: ['tv'],
+};
+export default config;
