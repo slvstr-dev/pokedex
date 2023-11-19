4ffc7d61b8f1c0fd57bc8e7580f62469fa7d42c9
Sylvester <hello@slvstr.dev>
2023-11-19 16:56:11 +0100

    Update SearchForm

--- src/components/form/SearchForm/SearchForm.tsx
+++ src/components/form/SearchForm/SearchForm.tsx
@@ -9,24 +9,20 @@ import { SearchBar } from '@/components/ui/SearchBar/SearchBar';
 
 export interface SearchFormProps {
   className?: string;
-  onSubmit?: () => void;
+  onChange: (query: string) => void;
 }
 
-export function SearchForm({ className, onSubmit }: SearchFormProps) {
-  const { register, handleSubmit, reset } = useForm<SearchFormSchema>({
+export function SearchForm({ className, onChange }: SearchFormProps) {
+  const { register, handleSubmit } = useForm<SearchFormSchema>({
     resolver: zodResolver(searchFormSchema),
   });
 
-  const handleFormSubmit = (data: SearchFormSchema) => {
-    console.log(data);
-
-    reset();
-
-    onSubmit?.();
+  const handleFormSubmit = ({ query }: SearchFormSchema) => {
+    onChange(query);
   };
 
   return (
-    <form onSubmit={handleSubmit(handleFormSubmit)} className={cn('flex flex-col', className)}>
+    <form onChange={handleSubmit(handleFormSubmit)} className={cn('flex flex-col', className)}>
       <SearchBar placeholder="Search Pokémon by name or number" register={register('query')} />
     </form>
   );
