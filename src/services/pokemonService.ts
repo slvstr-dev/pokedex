8705e761632eb8cddb2e6d323a342db9435c7b9b
Sylvester <hello@slvstr.dev>
2023-11-20 08:09:09 +0100

    Fix typo in Pokemon API URL

--- src/services/pokemonService.ts
+++ src/services/pokemonService.ts
@@ -23,7 +23,7 @@ export const getPokemonList = async () => {
 export const getPokemon = async (id: number) => {
   try {
     const response = await fetch(
-      `http:s//${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api/pokemon/${id}`,
+      `https://${process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL}/api/pokemon/${id}`,
     );
 
     if (!response.ok) {
