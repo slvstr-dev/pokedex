e43466774cdd158aabdb7731c3a0932a775dd25b
Sylvester <hello@slvstr.dev>
2024-02-09 11:28:22 +0100

    Add updated dependencies & required adjustments

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from 'next';
 import '@/assets/styles/globals.css';
 
 import { fonts } from '@/constants/fonts';
-import { Suspense, type PropsWithChildren } from 'react';
+import { type PropsWithChildren } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 import { NavigationBar } from '@/components/layout/NavigationBar/NavigationBar';
 import { Header } from '@/components/layout/Header/Header';
-import { SearchForm } from '@/components/form/SearchForm/SearchForm';
 import { Content } from '@/components/layout/Content/Content';
 
 export interface RootLayoutProps extends PropsWithChildren {}
